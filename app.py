34e5d68f47964b2532a547239c3cb24e7ca48fa7
Wilhan Nunes <85765131+wilhan-nunes@users.noreply.github.com>
2025-10-15 09:09:21 -0700

    adding new tracker
    Added tracking token for analytics.

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ git_hash = get_git_short_rev()
 repo_link = "https://github.com/wilhan-nunes/streamlit-Reverse-Metabolomics"
 
 # Add a tracking token
+html('<script async defer data-website-id="74bc9983-13c4-4da0-89ae-b78209c13aaf" src="https://analytics.gnps2.org/umami.js"></script>', width=0, height=0)
 html('<script async defer data-website-id="<your_website_id>" src="https://analytics.gnps2.org/umami.js"></script>', width=0, height=0)
 
 
