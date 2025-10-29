d3eeaba6fa293b3140bf725404cd522423934cf1
wilhan-nunes <wdgnsl@gmail.com>
2025-10-29 09:05:50 -0700

    bump version

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ mpl.rcParams['pdf.fonttype'] = 42
 mpl.rcParams['ps.fonttype'] = 42
 
 # TODO: Bump version
-app_version = "2025-08-22"
+app_version = "2025-10-29"
 git_hash = get_git_short_rev()
 repo_link = "https://github.com/wilhan-nunes/streamlit-Reverse-Metabolomics"
 
